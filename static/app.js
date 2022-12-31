fc2425df15decc7abba28c580961652ee8630b18
amanchopra1 <98337430+amanchopra1@users.noreply.github.com>
2022-12-31 20:44:07 +0530

    Update app.js

--- static/app.js
+++ static/app.js
@@ -46,7 +46,7 @@ class Chatbox {
         let msg1 = { name: "User", message: text1 }
         this.messages.push(msg1);
 
-        fetch('http://165.22.220.3/predict', {
+        fetch('http://sakha.chat/predict', {
             method: 'POST',
             body: JSON.stringify({ message: text1 }),
             mode: 'cors',
